4b9ec00d506be94b0e47a489c7812c363e693bfc
PrawinAyadi <49094521+PrawinAyadi@users.noreply.github.com>
2019-11-07 21:48:57 +0545

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+var http = require (http);
+var app = http.createServer(cb);
+
+function cb (req,res){
+    var body ='test http';
+    res.serHeader ("Content-Type", Text);
+    res.statusCode = 302
+    res.write(body);
+    res.end('hello');
+
+}
+public = 10 
+console.log(a);
+function test (b,a){
+"use strict";
+b=10;
+}
+
+test();
+app.listen('3010');
+console.log("server on");
+
+
+
+
+
+
+
+
+
+
+
+
